d8d159c95c34d540662a2a0963453dd9bb9f83b3
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2023-04-11 04:40:41 -0500

    .

---
+++ ToDoList/MVCCRUD/Models/Tarea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models
+{
+    public partial class Tarea
+    {
+        public int Id { get; set; }
+        public string? Descripcion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string Completada { get; set; }
+    }
+}
